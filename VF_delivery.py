baa78e59b947260c71a2747869f2a60ddc0a1e05
John Einselen VF <jeinselen@vectorform.com>
2022-11-15 23:09:23 -0500

    Major refactor of primary mesh exports
    Converting system to always directly select objects (unifying collection usage across export formats), universalise UVMap attribute conversion (untested)

--- VF_delivery.py
+++ VF_delivery.py
@@ -1,7 +1,7 @@
 bl_info = {
 	"name": "VF Delivery",
 	"author": "John Einselen - Vectorform LLC",
-	"version": (0, 7, 1),
+	"version": (0, 7, 2),
 	"blender": (3, 3, 1),
 	"location": "Scene > VF Tools > Delivery",
 	"description": "Quickly export selected objects to a specified directory",
@@ -34,6 +34,10 @@ class VFDELIVERY_OT_file(bpy.types.Operator):
 		# Set up local variables
 		location = bpy.context.scene.vf_delivery_settings.file_location
 		format = bpy.context.scene.vf_delivery_settings.file_type
+		combined = True if bpy.context.scene.vf_delivery_settings.file_group == "COMBINED" else False
+		file_format = "." + format.lower()
+		# Experimental UV map
+		uvmap_experimental = bpy.context.scene.vf_delivery_settings.uvmap_experimental
 
 		# Save current mode
 		mode = bpy.context.active_object.mode
@@ -41,193 +45,210 @@ class VFDELIVERY_OT_file(bpy.types.Operator):
 		# Override mode to OBJECT
 		bpy.ops.object.mode_set(mode='OBJECT')
 
-		# Check if an object is selected, if not, export collection instead
+		# Check if an object is selected, if not, convert selected collection into object selection
 		if bpy.context.object and bpy.context.object.select_get():
-			object_toggle = True
-			collection_toggle = False
-			button_title = bpy.context.active_object.name
+			file_name = bpy.context.active_object.name
 		else:
-			object_toggle = False
-			collection_toggle = True
-			button_title = bpy.context.collection.name
+			file_name = bpy.context.collection.name
+			for obj in bpy.context.collection.all_objects:
+				obj.select_set(True)
 
-		# Create file format based on pipeline selection
-		file_format = "." + format.lower()
+		if format != "CSV":
+			# Push an undo state (seems easier than trying to re-select previously selected non-mesh objects)
+			bpy.ops.ed.undo_push()
 
-		if format == "ABC":
-			if collection_toggle:
-				# Push an undo state (this was intended to reset the collection selection status, but doesn't really work)
-				bpy.ops.ed.undo_push()
-				for obj in bpy.context.collection.all_objects:
-					obj.select_set(True)
+			# Deselect any non-mesh objects
+			for obj in bpy.context.selected_objects:
+				if obj.type != "MESH":
+					obj.select_set(False)
 
-			bpy.ops.wm.alembic_export(
-				filepath=location + button_title + file_format,
-				check_existing=False, # Always overwrite existing files
-				start=0,
-				end=0,
-				xsamples=1,
-				gsamples=1,
-				selected=True,
-				visible_objects_only=False,
-				flatten=False,
-				uvs=True,
-				packuv=False, # Changed from default to prevent UV map alteration
-				normals=True,
-				vcolors=True, # Changed from default to include vertex colors
-				orcos=True,
-				face_sets=False,
-				subdiv_schema=False,
-				apply_subdiv=False,
-				curves_as_mesh=False,
-				use_instancing=True,
-				global_scale=1.0,
-				triangulate=False,
-				quad_method='SHORTEST_DIAGONAL',
-				ngon_method='BEAUTY',
-				export_hair=True,
-				export_particles=True,
-				export_custom_properties=False, # Changed from default
-				evaluation_mode='RENDER')
-
-			if collection_toggle:
-				# Undo the collection object selection status
-				bpy.ops.ed.undo()
-
-		elif format == "FBX":
+		# Begin primary export section (formats that support UV maps)
+		if format == "ABC" or format == "FBX" or format == "GLB":
 			# Push an undo state (seems easier than trying to re-select previously selected non-MESH objects)
+			# But this isn't working, right? Sigh...
+			# Track number of undo steps to retrace after export is complete
 			bpy.ops.ed.undo_push()
 			undo_steps = 1
 
-			if collection_toggle:
-				# Push an undo state to reset object selection later
-				bpy.ops.ed.undo_push()
-				for obj in bpy.context.collection.all_objects:
-					obj.select_set(True)
+			if bpy.context.preferences.addons['VF_delivery'].preferences.enable_uvmap_experimental and uvmap_experimental:
+				# Geometry Nodes (as of Blender 3.3) does not support UVMap export because they exist only as an incompatible named attribute
+				# To work around this issue all modifiers must be applied to meshes and any "UVmap" named attribute converted into a valid UV map
+				for obj in bpy.context.selected_objects:
+					if obj.type == "MESH":
+						# Set active
+						bpy.context.view_layer.objects.active = obj
 
-			# Geometry Nodes (as of Blender 3.3) does not support UVMap export because they exist only as a named attribute
-			# To work around this issue all modifiers must be applied to meshes and any "UVmap" named attribute converted into a valid UV map
+						# Apply all modifiers
+						if len(obj.modifiers) > 0:
+							bpy.ops.ed.undo_push()
+							undo_steps += 1
+							bpy.ops.object.apply_all_modifiers()
+
+						# Convert "UVMap" attribute to UV map data type
+						# If it exists and is selected by default...python API seems very limited here
+						if obj.data.attributes.get("UVMap") and obj.data.attributes.active.name == "UVMap":
+							bpy.ops.ed.undo_push()
+							undo_steps += 1
+							bpy.ops.geometry.attribute_convert(mode='UV_MAP')
+
+			# Loop through each of the selected objects
+			# But only set individual selections if file export is set to individual
+			# Otherwise loop once and exit (see the if statement at the very end)
 			for obj in bpy.context.selected_objects:
-				if obj.type == "MESH":
-					# Set active
-					bpy.context.view_layer.objects.active = obj
+				if not combined:
+					print("INDIVIDUAL")
+					print("selected: " + str(len(bpy.context.selected_objects)))
+					# deselect everything
+					for selobj in bpy.context.selected_objects:
+						selobj.select_set(False)
+					# select individual object
+					obj.select_set(True)
+					file_name = obj.name
+					print("selected: " + str(len(bpy.context.selected_objects)))
 
-					# Apply all modifiers
-					if len(obj.modifiers) > 0:
-						bpy.ops.ed.undo_push()
-						undo_steps += 1
-						bpy.ops.object.apply_all_modifiers()
+				if format == "ABC":
+					print("EXPORT: ABC")
+					bpy.ops.wm.alembic_export(
+						filepath=location + file_name + file_format,
+						check_existing=False, # Always overwrite existing files
+						start=0,
+						end=0,
+						xsamples=1,
+						gsamples=1,
+						selected=True,
+						visible_objects_only=False,
+						flatten=False,
+						uvs=True,
+						packuv=False, # Changed from default to prevent UV map alteration
+						normals=True,
+						vcolors=True, # Changed from default to include vertex colors
+						orcos=True,
+						face_sets=False,
+						subdiv_schema=False,
+						apply_subdiv=False,
+						curves_as_mesh=False,
+						use_instancing=True,
+						global_scale=1.0,
+						triangulate=False,
+						quad_method='SHORTEST_DIAGONAL',
+						ngon_method='BEAUTY',
+						export_hair=True,
+						export_particles=True,
+						export_custom_properties=False, # Changed from default
+						evaluation_mode='RENDER')
 
-					# Convert "UVMap" attribute to UV map data type (if it exists and is selected by default...python API seems pretty limited here?)
-					if obj.data.attributes.get("UVMap") and obj.data.attributes.active.name == "UVMap":
-						bpy.ops.ed.undo_push()
-						undo_steps += 1
-						bpy.ops.geometry.attribute_convert(mode='UV_MAP')
+				elif format == "FBX":
+					print("EXPORT: FBX")
+					bpy.ops.export_scene.fbx(
+						filepath=location + file_name + file_format,
+						check_existing=False, # Always overwrite existing files (dangerous...designed specifically for Unity delivery!)
+						use_selection=True,
+						use_visible=True,
+						use_active_collection=False, # This is now hardcoded, as we're converting collection selection into object selection manually above
 
-			bpy.ops.export_scene.fbx(
-				filepath=location + button_title + file_format,
-				check_existing=False, # Always overwrite existing files (dangerous...designed specifically for Unity delivery!)
-				use_selection=object_toggle, # If an object is selected, export only selected objects
-				use_visible=True,
-				use_active_collection=collection_toggle, # If an object isn't selected, export the active collection
+						global_scale=1.0, # 1.0
+						apply_unit_scale=True,
+						apply_scale_options='FBX_SCALE_NONE', # FBX_SCALE_NONE = All Local
+						use_space_transform=True,
+						axis_forward='-Z',
+						axis_up='Y',
+						object_types={'ARMATURE', 'CAMERA', 'EMPTY', 'LIGHT', 'MESH', 'OTHER'},
+						bake_space_transform=True, # True (this is "!experimental!")
 
-				global_scale=1.0, # 1.0
-				apply_unit_scale=True,
-				apply_scale_options='FBX_SCALE_NONE', # FBX_SCALE_NONE = All Local
-				use_space_transform=True,
-				axis_forward='-Z',
-				axis_up='Y',
-				object_types={'ARMATURE', 'CAMERA', 'EMPTY', 'LIGHT', 'MESH', 'OTHER'},
-				bake_space_transform=True, # True (this is "!experimental!")
+						use_mesh_modifiers=True, # Come back to this...manually trigger application of mesh modifiers and convert attributes to UV maps
+						use_mesh_modifiers_render=True,
+						mesh_smooth_type='OFF', # OFF = Normals Only
+						use_subsurf=False, # Seems unhelpful for realtime (until realtime supports live subdivision cross-platform)
+						use_mesh_edges=False,
+						use_tspace=False,
+						use_triangles=True, # This wasn't included in the "perfect" Unity settings, but seems logical?
+						use_custom_props=False,
 
-				use_mesh_modifiers=True, # Come back to this...manually trigger application of mesh modifiers and convert attributes to UV maps
-				use_mesh_modifiers_render=True,
-				mesh_smooth_type='OFF', # OFF = Normals Only
-				use_subsurf=False, # Seems unhelpful for realtime (until realtime supports live subdivision cross-platform)
-				use_mesh_edges=False,
-				use_tspace=False,
-				use_triangles=True, # This wasn't included in the "perfect" Unity settings, but seems logical?
-				use_custom_props=False,
+						use_armature_deform_only=True, # True
+						add_leaf_bones=False, # False
+						primary_bone_axis='X', # X Axis
+						secondary_bone_axis='Y', # Y Axis
+						armature_nodetype='NULL',
 
-				use_armature_deform_only=True, # True
-				add_leaf_bones=False, # False
-				primary_bone_axis='X', # X Axis
-				secondary_bone_axis='Y', # Y Axis
-				armature_nodetype='NULL',
+						bake_anim=True,
+						bake_anim_use_all_bones=True,
+						bake_anim_use_nla_strips=True,
+						bake_anim_use_all_actions=True,
+						bake_anim_force_startend_keying=True, # Some recommend False, but Unity may not load animations nicely without starting keyframes
+						bake_anim_step=1.0,
+						bake_anim_simplify_factor=1.0,
 
-				bake_anim=True,
-				bake_anim_use_all_bones=True,
-				bake_anim_use_nla_strips=True,
-				bake_anim_use_all_actions=True,
-				bake_anim_force_startend_keying=True, # Some recommend False, but Unity may not load animations nicely without starting keyframes
-				bake_anim_step=1.0,
-				bake_anim_simplify_factor=1.0,
+						path_mode='AUTO',
+						embed_textures=False,
+						batch_mode='OFF',
+						use_batch_own_dir=False,
+						use_metadata=True)
 
-				path_mode='AUTO',
-				embed_textures=False,
-				batch_mode='OFF',
-				use_batch_own_dir=False,
-				use_metadata=True)
+				elif format == "GLB":
+					print("EXPORT: GLB")
+					bpy.ops.export_scene.gltf(
+						filepath=location + file_name + file_format,
+						check_existing=False, # Always overwrite existing files (dangerous...designed specifically for ThreeJS delivery!)
+						export_format='GLB',
+						export_copyright='',
+
+						export_image_format='JPEG',
+						export_texcoords=True,
+						export_normals=True,
+						export_draco_mesh_compression_enable=True,
+						export_draco_mesh_compression_level=6,
+						export_draco_position_quantization=14,
+						export_draco_normal_quantization=10,
+						export_draco_texcoord_quantization=12,
+						export_draco_color_quantization=10,
+						export_draco_generic_quantization=12,
+
+						export_tangents=False,
+						export_materials='EXPORT',
+						export_colors=True,
+						use_mesh_edges=False,
+						use_mesh_vertices=False,
+						export_cameras=False,
+
+						use_selection=True,
+						use_visible=True,
+						use_renderable=True,
+						use_active_collection=False, # This is hardcoded now, as collections are converted manually to object selections above
+						use_active_scene=False,
+
+						export_extras=False,
+						export_yup=True,
+						export_apply=True,
+
+						export_animations=True,
+						export_frame_range=True,
+						export_frame_step=1,
+						export_force_sampling=True,
+						export_nla_strips=True,
+						export_def_bones=True, # Changed from default
+						export_optimize_animation_size=True, # Changed from default, may cause issues with stepped animations
+						export_current_frame=False,
+						export_skins=True,
+						export_all_influences=False,
+
+						export_morph=True,
+						export_morph_normal=True,
+						export_morph_tangent=False,
+
+						export_lights=False,
+						will_save_settings=False,
+						filter_glob='*.glb;*.gltf')
+
+				# Interrupt the loop if we're exporting all objects to the same file
+				if combined:
+					break
 
 			# Undo the previously completed object modifications
-			if collection_toggle:
-				bpy.ops.ed.undo()
 			for i in range(undo_steps):
 				bpy.ops.ed.undo()
 
-		elif format == "GLB":
-			bpy.ops.export_scene.gltf(
-				filepath=location + button_title + file_format,
-				check_existing=False, # Always overwrite existing files (dangerous...designed specifically for ThreeJS delivery!)
-				export_format='GLB',
-				export_copyright='',
-
-				export_image_format='JPEG',
-				export_texcoords=True,
-				export_normals=True,
-				export_draco_mesh_compression_enable=True,
-				export_draco_mesh_compression_level=6,
-				export_draco_position_quantization=14,
-				export_draco_normal_quantization=10,
-				export_draco_texcoord_quantization=12,
-				export_draco_color_quantization=10,
-				export_draco_generic_quantization=12,
-
-				export_tangents=False,
-				export_materials='EXPORT',
-				export_colors=True,
-				use_mesh_edges=False,
-				use_mesh_vertices=False,
-				export_cameras=False,
-
-				use_selection=object_toggle,
-				use_visible=True,
-				use_renderable=True,
-				use_active_collection=collection_toggle,
-				use_active_scene=False,
-
-				export_extras=False,
-				export_yup=True,
-				export_apply=True,
-
-				export_animations=True,
-				export_frame_range=True,
-				export_frame_step=1,
-				export_force_sampling=True,
-				export_nla_strips=True,
-				export_def_bones=True, # Changed from default
-				export_optimize_animation_size=True, # Changed from default, may cause issues with stepped animations
-				export_current_frame=False,
-				export_skins=True,
-				export_all_influences=False,
-
-				export_morph=True,
-				export_morph_normal=True,
-				export_morph_tangent=False,
-
-				export_lights=False,
-				will_save_settings=False,
-				filter_glob='*.glb;*.gltf')
+		# End primary exprt section (formats that support UV maps)
+		# Begin secondary export section (formats that do not support UV maps)
 
 		elif format == "STL":
 			# Push an undo state (seems easier than trying to re-select previously selected non-MESH objects)
@@ -285,6 +306,10 @@ class VFDELIVERY_OT_file(bpy.types.Operator):
 
 			# Reset timeline position
 			bpy.context.scene.frame_set(frame_current)
+
+		if format != "CSV":
+			# Undo the previously completed non-mesh object deselection
+			bpy.ops.ed.undo()
 
 		# Reset to original mode
 		bpy.ops.object.mode_set(mode=mode)
